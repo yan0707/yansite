5c865948bab8abd80fc8ce3ca2e750d377557a37
Yan <yanvvitor@gmail.com>
2024-10-29 16:34:39 -0300

    hoje fizemos o login

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from flask import Flask, render_template, request
 
 app= Flask(__name__)
 
+#Cria uma lista de usúario e senha, depois vamos pegar o DB
+usuarios = {
+    'yan' : '0907',
+    'Rayanna' : '1906' 
+}
 #definindo a rota pricipal do site
 @app.route('/')
 def home():
@@ -23,6 +28,21 @@ def música():
 @app.route("/principal")
 def principal():
     return render_template("principal.html")
-# Parte pricipaldo programa em Python
+
+    #verificar login
+@app.route('/verificar_login', methods=['POST'])
+def verificar_login():
+
+    #pegando o qe o usuario digitou no campo de entrada de user e senha
+    username = request.form['usuario']
+    password = request.form['senha']
+
+    #verifica se o usuario digitado está na lista e se a senha está correta
+    if username in usuarios and usuarios[username] == password:
+            return f"Bem-vindo, {username}!"
+    else:
+            return "Usuario ou senha inválidos."
+    
+# Parte pricipal do programa em Python
 if __name__== '__main__':
     app.run(debug=True)
